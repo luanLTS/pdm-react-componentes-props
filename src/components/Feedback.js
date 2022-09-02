a5324340d29967d47ecb33a8d52eb2ebcfaeb210
Luan Teixeira <luanlts@outlook.com>
2022-09-02 16:25:37 -0300

    feat(apostila04): fim da apostila 04

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,5 +1,18 @@
-export const FeedBack = () => (
+export const FeedBack = ({funcaoOk, funcaoNOk, textoOk, textoNOk}) => (
     <div className="d-flex justify-content-evenly m-2">
-        
+        <button
+            type="button"
+            className="btn btn-primary"
+            onclick={funcaoOk}
+        >
+            {textoOk}
+        </button>
+        <button
+            type="button"
+            className="btn btn-danger"
+            onclick={funcaoNOk}
+        >
+            {textoNOk}
+        </button>
     </div>
 )
--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import '@fortawesome/fontawesome-free/css/all.css'
 
 import { Pedido } from './components/Produto'
 import { Cartao } from './components/Cartao';
+import { FeedBack } from './components/Feedback'
+import { Alert } from 'bootstrap'
 
 
 const App = () => {
+
+    const textoOk = "Já chegou";
+    const textoNOk = "Não chegou";
+    const funcaoOk = () => Alert("Agradecemos a confirmação");
+    const funcaoNOk = () => Alert("Vamos verificar o ocorrido");
+    const feedback = <FeedBack
+        textoOk={textoOk}
+        textoNOk={textoNOk}
+        funcaoOk={funcaoOk}
+        funcaoNok={funcaoNOk}
+    />
     return (
         <div className='container border rounded mt-2'>
             <div className='row border-bottom m-2'>
@@ -16,46 +29,41 @@ const App = () => {
             </div>
 
             <div className="row">
-            <div className="col-sm-8 col-md-6 m-2">
-                {/*Entrada do primeiro pedido*/}
-                <Cartao cabecalho="22/04/2021">
-                    <Pedido
-                    icone="fa-solid fa-hdd fa-2x" 
-                    titulo="Kingston"
-                    descricao="SSD Kingston A400 - SATA"/>
-                </Cartao>
+                <div className="col-sm-12 col-md-6 my-2">
+                    {/*Entrada do primeiro pedido*/}
+                    <Cartao cabecalho="22/04/2021">
+                        <Pedido
+                            icone="fa-solid fa-hdd fa-2x"
+                            titulo="Kingston"
+                            descricao="SSD Kingston A400 - SATA" />
+                        {feedback}
+                    </Cartao>
+                </div>
+                <div className="col-sm-12 col-md-6 my-2">
+                    {/*Entrada do segundo pedido*/}
+                    <Cartao cabecalho="20/08/2022">
+                        <Pedido
+                            icone="fa-solid fa-book fa-2x"
+                            titulo="Livro"
+                            descricao="Concrete Mathematics - Donald Knuth" />
+                        {feedback}
+                    </Cartao>
+                </div>
+                <div className="col-sm-12 col-md-6 my-2">
+                    {/*Entrada do terceiro pedido*/}
+                    <Cartao cabecalho="21/08/2022">
+                        <Pedido
+                            data='21/08/2022'
+                            icone="fa-solid fa-laptop fa-2x"
+                            titulo="Notebook"
+                            descricao="Notebook Dell- 8Gb - i5" />
+                        {feedback}
+                    </Cartao>
+                </div>
             </div>
-        </div>
-
-        {/*linha para o segundo pedido */}
-        <div className="row">
-            <div className="col-sm-8 col-md-6 m-2">
-                {/*Entrada do segundo pedido*/} 
-                <Cartao cabecalho="20/08/2022">
-                    <Pedido 
-                    icone="fa-solid fa-book fa-2x" 
-                    titulo="Livro"
-                    descricao="Concrete Mathematics - Donald Knuth"/>
-                </Cartao>
-            </div>
-        </div>
-
-        {/*linha para o terceiro pedido */}
-        <div className="row">
-            <div className="col-sm-8 col-md-6 m-2">
-                {/*Entrada do terceiro pedido*/}
-                <Cartao cabecalho="21/08/2022">
-                    <Pedido 
-                    data='21/08/2022' 
-                    icone="fa-solid fa-laptop fa-2x" 
-                    titulo="Notebook"
-                    descricao="Notebook Dell- 8Gb - i5"/>      
-                </Cartao>
-            </div>
-        </div>
 
         </div>
-        
+
     )
 }
-ReactDom.createRoot(document.querySelector("#root")).render(<App/>);
+ReactDom.createRoot(document.querySelector("#root")).render(<App />);
